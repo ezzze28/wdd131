a2eba0f0b62997868345522ea84c4f42e44fdf67
ezzze28 <papitomiret@gmail.com>
2026-09-05 17:29:05 -0300

    Add initial HTML structure, JavaScript for date handling, and CSS styles
    - Created index.html with a header, main content, and footer sections.
    - Added JavaScript file (getdates.js) to display the current year and last modified date.
    - Developed base.css for styling the layout, including responsive design and hover effects.

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYearSpan = document.getElementById("currentyear");
+const today = new Date();
+currentYearSpan.textContent = today.getFullYear();
+
+const lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
